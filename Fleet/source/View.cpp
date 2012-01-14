e9e9ce57837cf209c2db114f638a1b5ce9cd410c
Magnus Spang <magnus.spang@gmail.com>
2012-01-14 15:19:27 +0100

    Adding empty view

---
+++ Fleet/source/View.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "../include/View.h"
+
+View::View()
+{
+  std::cout << "View constructor" << std::endl;
+}
--- Fleet/source/main.cpp
+++ Fleet/source/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "../include/Ship.h"
+#include "../include/View.h"
 
 int main()
 {
   std::cout << "A dummy main" << std::endl;
+
+  View view;
 
   std::cin.ignore();
   return 0;
